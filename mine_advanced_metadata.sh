48cee3d7c34d7c6d10983afb3344fb82ee9eff9c
istewart6 <istewart6@gatech.edu>
2016-04-14 22:26:25 -0400

    get them bash files girl

---
+++ mine_advanced_metadata.sh
@@ -0,0 +1,2 @@
+# mine metadata from MusicBrainz and output to file
+python mine_advanced_metadata.py > mine_advanced_metadata_output.txt
---
+++ mine_metadata.sh
@@ -0,0 +1,2 @@
+# mine metadata from MusicBrainz and output to file
+python mine_metadata.py > mine_metadata_output.txt
